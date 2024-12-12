7c14372c138cae62abae21e31211e8b48de02e0c
Edvard Bakken <edvard.bakken@bekk.no>
2024-12-12 16:02:26 +0100

    hode skulder kne og tå

--- nextjs-app/app/components/Header.tsx
+++ nextjs-app/app/components/Header.tsx
@@ -5,7 +5,7 @@ import bekkIcon from "../../static/Bekk_Logo.svg";
 
 export default function Header() {
   return (
-    <header className="fixed z-50 h-24 inset-0 bg-white/80 flex items-center backdrop-blur-lg">
+    <header className="z-50 h-24 inset-0 bg-white/80 flex items-center backdrop-blur-lg">
       <div className="container py-6 sm:px-6">
         <div className="flex items-center justify-between gap-5">
           <Link
