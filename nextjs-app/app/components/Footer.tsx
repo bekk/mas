1fe9a93a46b24eadb8ba7fe7d9033561fae0deb9
Edvard Bakken <edvard.bakken@bekk.no>
2024-12-12 15:10:21 +0100

    Make footer look like header, but gray

--- nextjs-app/app/components/Footer.tsx
+++ nextjs-app/app/components/Footer.tsx
@@ -1,24 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import maasIcon from "../../static/maas-logo.svg";
+import bekkIcon from "../../static/Bekk_Logo.svg";
+
 export default function Footer() {
   return (
     <footer className="bg-gray-50 border-gray-100 border-t">
-      <div className="container">
-        <div className="flex flex-col items-center py-28 lg:flex-row">
-          <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
-            Built with Sanity + Next.js.
-          </h3>
-          <div className="flex flex-col gap-3 items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
-            <a
-              href="https://github.com/sanity-io/sanity-template-nextjs-clean"
-              className="rounded-full flex gap-2 items-center bg-black hover:bg-red-500 focus:bg-cyan-500 py-3 px-6 text-white transition-colors duration-200"
-              target="_blank"
-              rel="noopener noreferrer"
+      <div className="container py-6 sm:px-6">
+        <div className="flex items-center justify-between gap-5">
+          <Link
+            className="rounded-full flex gap-2 items-center hover:bg-red-500 focus:bg-cyan-500 p-1 sm:py-3 sm:px-6 text-white transition-colors duration-200"
+            href="/"
+          >
+            <Image priority src={maasIcon} alt="Back to home" />
+          </Link>
+
+          <nav className="">
+            <ul
+              role="list"
+              className="flex items-center gap-4 md:gap-6 leading-5 text-sm md:text-base tracking-tight font-normal"
             >
-              View on GitHub
-            </a>
-            <a href="https://nextjs.org/docs" className="mx-3 hover:underline">
-              Read Next.js Documentation
-            </a>
-          </div>
+              <li>
+                <Link
+                  className="rounded-full flex gap-2 items-center hover:bg-red-500 focus:bg-cyan-500 p-1 sm:py-3 sm:px-6 text-white transition-colors duration-200"
+                  href="https://bekk.no"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image priority src={bekkIcon} alt="Bekk Consulting AS" />
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </footer>
