66dcc32776462df85e202fffa12cfd5f4bc6e1f1
Andreas <andreas.boestrand@gmail.com>
2024-12-12 15:26:32 +0100

    WIP team page

--- nextjs-app/app/team/[team]/page.tsx
+++ nextjs-app/app/team/[team]/page.tsx
@@ -15,5 +15,32 @@ export default async function Page(props: Props) {
   if (team == null) {
     return null;
   }
-  return <h2>{team.name}</h2>;
+  return (
+    <div>
+      <h2>{team.name}</h2>
+      {team.goals.map((goal, index) => {
+        return (
+          <div key={goal._id} className="max-w-[40rem] m-12 bg-red">
+            <h2>Mål {index + 1}</h2>
+            <h3>{goal.objective}</h3>
+            {goal.keyResults.map((kr, krIndex) => {
+              return (
+                <div key={kr._id}>
+                  <h2>KR {krIndex + 1}</h2>
+                  <h3>{kr.description}</h3>
+                  <div
+                    style={{
+                      height: "16px",
+                      width: `${kr.progress}%`,
+                      backgroundColor: "gray",
+                    }}
+                  ></div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
